eedba219bab409c92c1b364c02fa8853263e864d
Snehali Teltumbde <teltumbde.snehali.works@gmail.com>
2023-08-30 17:45:40 +0530

    Memories Project Done

---
+++ client/src/index.js
@@ -0,0 +1,36 @@
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import reducers from "./reducers";
+// import App from "./App";
+
+// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+root.render(
+  // <StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  /* </StrictMode> */
+);
